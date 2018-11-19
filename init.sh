2d835864d6a4b32d38710ccfd8f877f55c8af26e
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2018-11-20 08:54:21 +1300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
-
+# Set path 
+cd /epeverlogger
 
 # Mount virtual Device 
 socat pty,link=/dev/ttyUSB21,unlink-close=0,raw,echo=0 tcp:192.168.9.11:8088&
 chmod 777 /dev/ttyUSB21
 
-
-cd /epeverlogger
-
+# Main loop 
 while :
 do
+        # sleep 0 
         php logger.php
 done
