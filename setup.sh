09c3167b9e148cc099f7ff5ebc05e822d77277cc
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2018-11-20 10:35:13 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-# Setup - None
+# Setup
+
 # Get influxDB PHP 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 composer require influxdb/influxdb-php
