9755caa6f164379477afd076d99d1941789cd9fe
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2018-11-28 17:50:39 +1300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,17 +2,27 @@
 cd /epeverlogger
 
 # Mount virtual Device 
-socat pty,link=/dev/ttyUSB21,unlink-close=0,raw,echo=0 tcp:192.168.9.11:8088&
-# sleep 5 
-# chmod 777 /dev/ttyUSB21
+# If no $EBOX_IP then default to 192.168.9.11
+
+if [ -z "$EBOX_IP" ]
+        then export EBOX_IP=192.168.9.11
+fi
+
+socat pty,link=/dev/ttyUSB21,unlink-close=0,raw,echo=0 tcp:$EBOX_IP:8088&
 
 # echo date and first logger run
 date
 php logger.php 
 
 # Main loop - hide output
+# Delay set to $POLLING_DELAY
 while :
 do
-        # sleep 0 
+        if [ -z "$POLLING_DELAY" ]
+                then sleep 0
+                else sleep $POLLING_DELAY
+        fi
+        
         php -q logger.php > /dev/null
+
 done
